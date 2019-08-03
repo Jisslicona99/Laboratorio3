d5f88b2da74b84fe0f74230c3040d3daca3b2c72
Jisslicona99 <53011909+Jisslicona99@users.noreply.github.com>
2019-08-03 00:41:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+const PORT = 80;
+var app = express();
+app.listen(PORT, function(){
+    console.log("My http server listening on port " + PORT + "...");
+});
+app.get('/foo', function(req, res){
+    console.log('Hello, I am foo.');
+});
